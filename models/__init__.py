0cc993d34bb829a76fdcd59e3363ff79277b37f0
abdelmalek <abdelmalek@gmail.com>
2023-09-20 01:53:55 -0700

    fix

--- models/__init__.py
+++ models/__init__.py
@@ -4,8 +4,8 @@ from models.engine.file_storage import FileStorage
 from models.engine.db_storage import DBStorage
 from os import getenv
 
-StorageType = getenv('HBNB_TYPE_STORAGE')
-if StorageType == 'db':
+storage_type = getenv('HBNB_TYPE_STORAGE')
+if storage_type == 'db':
     storage = DBStorage()
 else:
     storage = FileStorage()
