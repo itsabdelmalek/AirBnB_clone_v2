5fc39a53accce305151e837212a14a88bdad1ea5
abdelmalek <abdelmalek@gmail.com>
2023-09-20 04:29:59 -0700

    update

--- tests/test_models/test_amenity.py
+++ tests/test_models/test_amenity.py
@@ -1,11 +1,19 @@
 #!/usr/bin/python3
-""" """
-from tests.test_models.test_base_model import test_basemodel
+"""Defines unnittests for models/amenity.py"""
+import os
+import MySQLdb
+import unittest
+from datetime import datetime
+from models.base_model import BaseModel, Base
 from models.amenity import Amenity
+from models.engine.db_storage import DBStorage
+from models.engine.file_storage import FileStorage
+from sqlalchemy.exc import OperationalError
+from sqlalchemy.orm import sessionmaker
 
 
 class test_Amenity(test_basemodel):
-    """ """
+    """Unittests for testing the Amenity class"""
 
     def __init__(self, *args, **kwargs):
         """ """
