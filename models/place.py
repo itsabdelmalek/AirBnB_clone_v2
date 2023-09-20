1517d8d6f7eb546d2b8faa63b5920c8b560f658f
abdelmalek <abdelmalek@gmail.com>
2023-09-20 00:53:11 -0700

    fix

--- models/place.py
+++ models/place.py
@@ -44,7 +44,7 @@ class Place(BaseModel, Base):
         price_by_night = Column(Integer, nullable=False, default=0)
         latitude = Column(Float, nullable=True)
         longitude = Column(Float, nullable=True)
-        reviews = relationship("Review", backref="place", cascade="all,delete")
+        reviews = relationship("Review", backref="place", cascade="delete")
         amenities = relationship("Amenity", secondary=place_and_amenity,
                                  viewonly=False,
                                  back_populates="place_amenities")
--- models/review.py
+++ models/review.py
@@ -12,11 +12,6 @@ class Review(BaseModel, Base):
     """ Review classto store review information """
     __tablename__ = "reviews"
 
-    if storage_type == "db":
-        place_id = Column(String(60), ForeignKey("places.id"), nullable=False)
-        user_id = Column(String(60), ForeignKey("users.id"), nullable=False)
-        text = Column(Text(1024), nullable=False)
-    else:
-        place_id = ""
-        user_id = ""
-        text = ""
+    text = Column(String(1024), nullable=False)
+    place_id = Column(String(60), ForeignKey("places.id"), nullable=False)
+    user_id = Column(String(60), ForeignKey("users.id"), nullable=False)
