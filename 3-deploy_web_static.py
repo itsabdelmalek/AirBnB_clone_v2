e3edd10e976f9dd838cd9f8ba1b650a269933fd3
abdelmalek <abdelmalek@gmail.com>
2023-10-05 03:40:50 -0700

    fix

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -4,9 +4,9 @@ This script creates and distributes an archive to web servers.
 """
 import os.path
 from datetime import datetime
-from fabric.api import env, local, run, put
+from fabric.api import env, local, put, run
 
-env.hosts = ["34.232.72.195", "34.229.55.94"]
+env.hosts = ["104.196.168.90", "35.196.46.172"]
 
 
 def do_pack():
@@ -47,3 +47,36 @@ def do_deploy(archive_path):
         return False
     file = archive_path.split("/")[-1]
     name = file.split(".")[0]
+
+    if put(archive_path, "/tmp/{}".format(file)).failed is True:
+        return False
+    if run("rm -rf /data/web_static/releases/{}/".
+           format(name)).failed is True:
+        return False
+    if run("mkdir -p /data/web_static/releases/{}/".
+           format(name)).failed is True:
+        return False
+    if run("tar -xzf /tmp/{} -C /data/web_static/releases/{}/".
+           format(file, name)).failed is True:
+        return False
+    if run("rm /tmp/{}".format(file)).failed is True:
+        return False
+    if run("mv /data/web_static/releases/{}/web_static/* "
+           "/data/web_static/releases/{}/".format(name, name)).failed is True:
+        return False
+    if run("rm -rf /data/web_static/releases/{}/web_static".
+           format(name)).failed is True:
+        return False
+    if run("rm -rf /data/web_static/current").failed is True:
+        return False
+    if run("ln -s /data/web_static/releases/{}/ /data/web_static/current".
+           format(name)).failed is True:
+        return False
+
+
+def deploy():
+    """Create and distribute an archive to a web server."""
+    archive_path = do_pack()
+    if archive_path is None:
+        return False
+    return do_deploy(archive_path)
