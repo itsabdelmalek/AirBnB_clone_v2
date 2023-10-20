8ef124cd19abe52241f21502ab9bc870d07d5fe0
abdelmalek <abdelmalek@gmail.com>
2023-10-20 09:18:12 -0700

    fix

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -4,10 +4,12 @@ This is a Flask web application.
 """
 
 from models import storage
+from models.state import State
+from models.city import City
 from flask import Flask
 from flask import render_template
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder="templates")
 
 
 @app.route("/cities_by_states", strict_slashes=False)
@@ -16,8 +18,12 @@ def cities_by_states():
     Display a HTML page with the list of all State objects
     and their linked City objects.
     """
-    states = storage.all("State")
-    return render_template("8-cities_by_states.html", states=states)
+    states = storage.all(State)
+    states = dict(sorted(states.items(), key=lambda item: item[1].name))
+    cities = storage.all(City)
+    cities = dict(sorted(cities.items(), key=lambda item: item[1].name))
+    return render_template("8-cities_by_states.html", states=states,
+                           cities=cities)
 
 
 @app.delete_fjson_appcontext
