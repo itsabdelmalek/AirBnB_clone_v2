7d0bc13a38aad6c560020f80a2768987f93f1f7a
abdelmalek <abdelmalek@gmail.com>
2023-10-20 08:20:42 -0700

    fix

--- models/state.py
+++ models/state.py
@@ -1,27 +1,33 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
 from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String
-from sqlalchemy.orm import relationship
-from models.city import City
-from os import getenv
 
-storage_type = getenv("HBNB_TYPE_STORAGE")
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.orm import relationship
+
+import os
+
+env_value = os.environ.get("HBNB_TYPE_STORAGE")
 
 
 class State(BaseModel, Base):
-    """ State class """
+    """State class"""
 
     __tablename__ = "states"
-    name = Column(String(128), nullable=False)
-    cities = relationship("City",  backref="state", cascade="delete")
+    if env_value == "db":
+        name = Column(String(128), nullable=False)
+        cities = relationship("City", backref="state")
+    else:
+        name = ""
 
-    if getenv("HBNB_TYPE_STORAGE") != "db":
         @property
         def cities(self):
-            """Get a list of all related City objects."""
+            from models.__init__ import storage
+            from models.city import City
+
             city_list = []
-            for city in models.storage.all(City).values():
-                if city.state_id == self.id:
-                    city_list.append(city)
+            d_storage = storage.all(City)
+            for key, value in d_storage.items():
+                if self.id == value.state_id:
+                    city_list.append(value)
             return city_list
