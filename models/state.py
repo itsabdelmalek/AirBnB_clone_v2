c009c5318d6142f8b7ccc5060753db2d6482e123
abdelmalek <abdelmalek@gmail.com>
2023-10-20 04:47:28 -0700

    fix

--- models/state.py
+++ models/state.py
@@ -21,7 +21,7 @@ class State(BaseModel, Base):
         def cities(self):
             """Get a list of all related City objects."""
             city_list = []
-            for city in list(models.storage.all(City).values()):
+            for city in models.storage.all(City).values():
                 if city.state_id == self.id:
                     city_list.append(city)
             return city_list
