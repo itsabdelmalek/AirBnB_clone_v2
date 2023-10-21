2ba657a84dc82dab3499f7e343276ce5586ff730
abdelmalek <abdelmalek@gmail.com>
2023-10-21 07:15:46 -0700

    fix

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -4,44 +4,29 @@ This is a Flask web application.
 """
 
 from models import storage
-from models.state import State
-from models.city import City
 from flask import Flask
 from flask import render_template
 
 app = Flask(__name__)
 
 
-@app.route("/states", strict_slashes=False)
-def states():
-    """
-    Displays a HTML page with the list of all State objects
-    present in DBStorage sorted by name (A->Z)
-    """
-    states = storage.all(State).values()
-    return render_template("9-states.html", state=states)
-
-
-@app.route("/states/<id>", strict_slashes=False)
-def states_id(id):
-    """
-    Displays a HTML page with the details of a specific State
-    including linked City objects.
-    """
-    state = storage.get(State, id)
-    if state:
-        cities = sorted(state.cities, key=lambda x: x.name)
-        return render_template("9-states.html", state=state, cities=cities)
-    else:
-        return render_template("9-states.html", not_found=True)
-
-
 @app.teardown_appcontext
-def teardown(exception):
+def teardown_context(exception):
     """
     Removes the current SQLAlchemy session.
     """
     storage.close()
+
+
+@app.route("/states", strict_slashes=False)
+@app.route("/states/<id>", strict_slashes=False)
+def state_id(id=None):
+    """Render state by id
+    """
+    states = storage.all("State").values()
+    cities = storage.all("City").values()
+    return render_template("9-states.html",
+                           states=states, cities=cities, id=id)
 
 
 if __name__ == "__main__":
