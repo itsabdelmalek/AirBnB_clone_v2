befca954f7d401bcfdad855aec5d6bbe297c1f2c
abdelmalek <abdelmalek@gmail.com>
2023-10-21 08:31:21 -0700

    fix

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -26,16 +26,15 @@ def states():
 @app.route("/states/<id>", strict_slashes=False)
 def state_id(id):
     """Displays an HTML page with info about <id>, if it exists."""
-    states = dict(sorted(states.items(), key=lambda item: item[1].name))
+    states = dict(sorted(storage.all(State).items(), key=lambda item: item[1].name))
     for key, state in states.items():
         if state.id == id:
-            c = storage.all(City)
+            s_all = storage.all(City)
             cities = {}
-            for key, city in c.items():
+            for key, city in s_all.items():
                 if state.id == city.state_id:
                     cities[key] = city
-            cities = dict(sorted(cities.items(),
-                                 key=lambda item: item[1].name))
+            cities = dict(sorted(cities.items(), key=lambda item: item[1].name))
             return render_template("9-states.html", name=state.name,
                                    not_found=False, data=cities, id=1)
     return render_template("9-states.html", not_found=True, data=states)
